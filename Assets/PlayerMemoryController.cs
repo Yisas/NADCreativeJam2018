01f98dc0c7b242a1f5ba823be764154700cfdc86
Jesus Imery <yisas_0929@hotmail.com>
2018-08-25 01:43:05 -0400

    #2 Occupying the empty memories first

--- Assets/PlayerMemoryController.cs
+++ Assets/PlayerMemoryController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerMemoryController : MonoBehaviour {
 
     public enum MemoryTypes { None, Pitfall};
+    private readonly int maxNumberOfMemories = 3;       // Mamimum amount of memories allowed
 
     public float minMemoryChangeTimer;
     public float maxMemoryChangeTimer;
     private float memoryChangeTimer = 0;
+
+    // How many memories you have
+    private int memoryCount = 0;
 
     private MemoryTypes[] memories = new MemoryTypes[3];
 
@@ -33,8 +37,20 @@ public class PlayerMemoryController : MonoBehaviour {
 
     void AssignNewMemory(MemoryTypes memory)
     {
-        // Choose which memory index to change
-        int memoryIndexToChange = Random.Range(0, 2);
+        int memoryIndexToChange = 0;
+
+        // If the memory stack is not full, occupy an empty one...
+        if (memoryCount < maxNumberOfMemories)
+        {
+            memoryIndexToChange = memoryCount;
+            memoryCount++;
+        }
+        // ...  else change a random one
+        else
+        {
+            memoryIndexToChange = Random.Range(0, 2);
+        }
+
         MemoryCanvasController.instance.ChangeMemory(memoryIndexToChange);
     }
 }
