8f50892b18205f2a057c5efbfc69db5f87360cdc
Jesus Imery <yisas_0929@hotmail.com>
2018-08-25 01:55:48 -0400

    #4 Display of multiple memory types

--- Assets/MemoryCanvasController.cs
+++ Assets/MemoryCanvasController.cs
@@ -3,36 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MemoryCanvasController : MonoBehaviour {
+public class MemoryCanvasController : MonoBehaviour
+{
 
     public static MemoryCanvasController instance;
 
     public Image[] memorySprites;
     public Sprite pitfallMemorySprite;
+    public Sprite climbingMemorySprite;
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
     }
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
     /// <summary>
     /// 
     /// </summary>
     /// <param name="memoryIndexToChange">Starts with 0</param>
-    public void ChangeMemory(int memoryIndexToChange)
+    public void ChangeMemory(int memoryIndexToChange, PlayerMemoryController.MemoryTypes memoryType)
     {
-        memorySprites[memoryIndexToChange].sprite = pitfallMemorySprite;
+        switch (memoryType)
+        {
+            case PlayerMemoryController.MemoryTypes.None:
+                memorySprites[memoryIndexToChange].sprite = null;
+                break;
+
+            case PlayerMemoryController.MemoryTypes.Pitfall:
+                memorySprites[memoryIndexToChange].sprite = pitfallMemorySprite;
+                break;
+            case PlayerMemoryController.MemoryTypes.Climb:
+                memorySprites[memoryIndexToChange].sprite = climbingMemorySprite;
+                break;
+        }
     }
+
 }
