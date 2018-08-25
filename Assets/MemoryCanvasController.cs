32d75032bde0fdc303f6f8155c79f61eaf3f6553
Jesus Imery <yisas_0929@hotmail.com>
2018-08-25 01:36:10 -0400

    #4 Placeholder UI for representing memories

---
+++ Assets/MemoryCanvasController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryCanvasController : MonoBehaviour {
+
+    public static MemoryCanvasController instance;
+
+    public Image[] memorySprites;
+    public Sprite pitfallMemorySprite;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="memoryIndexToChange">Starts with 0</param>
+    public void ChangeMemory(int memoryIndexToChange)
+    {
+        memorySprites[memoryIndexToChange].sprite = pitfallMemorySprite;
+    }
+}
