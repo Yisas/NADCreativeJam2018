4e6293bc87c9b12f85b512fa0c5f3bcc2e1f4a4c
joff <lplb@outlook.com>
2018-08-25 12:02:06 -0400

    Adding mockup model for buddy AI

---
+++ Assets/Scripts/AI/BuddyAIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyAIController : MonoBehaviour {
+    public GameObject target;
+    public float damping = 3;
+
+    void FixedUpdate()
+    {
+        this.transform.position = new Vector3(Mathf.Lerp(this.transform.position.x, target.transform.position.x, Time.deltaTime * damping),
+                                               Mathf.Lerp(this.transform.position.y, target.transform.position.y, Time.deltaTime * damping), 0);
+    }
+}
