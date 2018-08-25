715e7296289e9dd4e89c5ab492b1a6cccc7d907f
Jesus Imery <yisas_0929@hotmail.com>
2018-08-25 00:31:22 -0400

    #1 First moving camera, 2D-3D collision illusion

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        bool playerIsMovingCharacter = Mathf.Abs(horizontalInput) > 0;
+
+        if (playerIsMovingCharacter)
+        {
+            transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0));
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
