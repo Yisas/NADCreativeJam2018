6357f88c7ab1ecb224a3dab4ed4681ca40d06123
Jesus Imery <yisas_0929@hotmail.com>
2018-08-26 11:17:58 -0400

    #25 First pass at stepping sounds

--- Assets/Scripts/Animations/AnimatorCommunicator.cs
+++ Assets/Scripts/Animations/AnimatorCommunicator.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class AnimatorCommunicator : MonoBehaviour
 {
+    public AudioClip stepSound;
+
+    private AudioSource audioSource;
     private PlayerController playerController;
 
     private void Start()
     {
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void TriggerJump()
     {
         playerController.TriggerJump();
+    }
+
+    public void Step()
+    {
+        audioSource.PlayOneShot(stepSound);
     }
 }
