4f2d993ea891b08f460cb858396458ac5034711e
Jesus Imery <yisas_0929@hotmail.com>
2018-08-26 08:19:12 -0400

    #22 #24 First pass at tutorial prompts by zone

---
+++ Assets/Scripts/Game Management/TutorialPrompt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPrompt : MonoBehaviour
+{
+    public string tutorialMessageToDisplay;
+    private BuddyAIController buddyAI;
+
+    // Use this for initialization
+    void Start()
+    {
+        buddyAI = GameObject.FindGameObjectWithTag("BuddyAI").GetComponent<BuddyAIController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            buddyAI.DisplayMessage(tutorialMessageToDisplay);
+        }
+    }
+
+}
